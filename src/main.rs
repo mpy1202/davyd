d969654ec6839fc03ae4b38a26cb884707726b28
mpy1202 <mattyoung905@gmail.com>
2024-07-08 21:00:16 +0100

    1st test release

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 use leptos::*;
 
-fn main() {
-    mount_to_body(|| view! { <div class="centre"><button><h1>"Consider"</h1></button></div> })
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    
+    view! {
+        <div class="centre">
+            <button
+                on:click=move |_| {
+                    // on stable, this is set_count.set(3);
+                    set_count.update(|n| *n += 1);
+                }
+            >
+                <h1>"Consider " {move || count()}</h1>            
+                // on stable, this is move || count.get();
+                
+            </button>
+        </div>
+    }
 }
+fn main() {
+    console_error_panic_hook::set_once();
+    leptos::mount_to_body(|| view! { <App/> })
+}  
+
+//<div class="centre"><button></button></div> })
